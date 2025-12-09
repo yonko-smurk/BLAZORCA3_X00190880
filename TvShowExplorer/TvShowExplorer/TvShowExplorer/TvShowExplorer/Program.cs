292e9fa82f9db87c00eb2f8afd001f779ba391eb
yonko-smurk <x00190880@mytudublin.ie>
2025-12-09 23:21:47 +0000

    Fix router NotFound handling and clean Blazor assets

--- TvShowExplorer/TvShowExplorer/TvShowExplorer/TvShowExplorer/Program.cs
+++ TvShowExplorer/TvShowExplorer/TvShowExplorer/TvShowExplorer/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TvShowExplorer.Services;
 using TvShowExplorer;
 using TvShowExplorer.Services;
 
